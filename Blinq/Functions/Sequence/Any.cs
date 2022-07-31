1ce2683798f72f34ebe4d3922b83eb08dca09932
Kornei Dontsov <KorneiDontsov@gmail.com>
2022-07-31 20:34:13 +0300

    Implement Any

---
+++ Blinq/Functions/Sequence/Any.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace Blinq;
+
+readonly struct AnyFoldFunc<T>: IFoldFunc<T, bool> {
+   [MethodImpl(MethodImplOptions.AggressiveInlining)]
+   [SuppressMessage("ReSharper", "RedundantAssignment")]
+   public bool Invoke (T item, ref bool accumulator) {
+      accumulator = true;
+      return true;
+   }
+}
+
+public static partial class Sequence {
+   public static bool Any<T, TIterator> (this in Sequence<T, TIterator> sequence) where TIterator: IIterator<T> {
+      return sequence.Iterator.Fold(false, new AnyFoldFunc<T>());
+   }
+}
