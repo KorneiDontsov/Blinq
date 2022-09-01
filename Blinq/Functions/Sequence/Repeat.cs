c62a871479ea981330508eb7ce1da9a0ee7c14b8
Kornei Dontsov <KorneiDontsov@gmail.com>
2022-09-01 14:04:12 +0300

    Repeat

---
+++ Blinq/Functions/Sequence/Repeat.cs
@@ -0,0 +1,30 @@
+namespace Blinq;
+
+public struct RepeatIterator<T>: IIterator<T> {
+   readonly T Item;
+   int Count;
+
+   [MethodImpl(MethodImplOptions.AggressiveInlining)]
+   public RepeatIterator (T item, int count) {
+      Item = item;
+      Count = count;
+   }
+
+   [MethodImpl(MethodImplOptions.AggressiveInlining)]
+   public TAccumulator Fold<TAccumulator, TFoldFunc> (TAccumulator seed, TFoldFunc func) where TFoldFunc: IFoldFunc<T, TAccumulator> {
+      while (Count > 0) {
+         --Count;
+         if (func.Invoke(Item, ref seed)) break;
+      }
+
+      return seed;
+   }
+}
+
+public static partial class Sequence {
+   [MethodImpl(MethodImplOptions.AggressiveInlining)]
+   public static Sequence<T, RepeatIterator<T>> Repeat<T> (T item, int count) {
+      if (count < 0) throw new ArgumentOutOfRangeException(nameof(count), count, null);
+      return new Sequence<T, RepeatIterator<T>>(new RepeatIterator<T>(item, count), count);
+   }
+}
