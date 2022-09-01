997364a00e05a5a75752333e1426beb6770a655b
Kornei Dontsov <KorneiDontsov@gmail.com>
2022-09-01 14:04:06 +0300

    Last

---
+++ Blinq/Functions/Sequence/Last.cs
@@ -0,0 +1,19 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace Blinq;
+
+readonly struct LastFoldFunc<T>: IFoldFunc<T, Option<T>> {
+   [MethodImpl(MethodImplOptions.AggressiveInlining)]
+   [SuppressMessage("ReSharper", "RedundantAssignment")]
+   public bool Invoke (T item, ref Option<T> accumulator) {
+      accumulator = Option.Value(item);
+      return false;
+   }
+}
+
+public static partial class Sequence {
+   [MethodImpl(MethodImplOptions.AggressiveInlining)]
+   public static Option<T> Last<T, TIterator> (this in Sequence<T, TIterator> sequence) where TIterator: IIterator<T> {
+      return sequence.Iterator.Fold(Option<T>.None, new LastFoldFunc<T>());
+   }
+}
