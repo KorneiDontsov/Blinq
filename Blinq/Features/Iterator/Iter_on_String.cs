a003372486b115b4890c0b933070dca65679975e
Kornei Dontsov <korneidontsov@gmail.com>
2023-02-19 17:38:08 +0300

    Add StringIterator

---
+++ Blinq/Features/Iterator/Iter_on_String.cs
@@ -0,0 +1,50 @@
+namespace Blinq;
+
+/// <inheritdoc />
+/// <summary>A string iterator.</summary>
+public struct StringIterator: IIterator<char> {
+   readonly string Str;
+   int Index;
+
+   [MethodImpl(MethodImplOptions.AggressiveInlining)]
+   internal StringIterator (string str) {
+      Str = str;
+   }
+
+   /// <inheritdoc />
+   [MethodImpl(MethodImplOptions.AggressiveInlining)]
+   public bool TryPop (out char item) {
+      if (Index < Str.Length) {
+         item = Str[Index++];
+         return true;
+      } else {
+         item = default;
+         return false;
+      }
+   }
+
+   /// <inheritdoc />
+   [MethodImpl(MethodImplOptions.AggressiveInlining)]
+   public TAccumulator Fold<TAccumulator, TFold> (TAccumulator seed, TFold fold) where TFold: IFold<char, TAccumulator> {
+      foreach (var item in Str.AsSpan(Index)) {
+         ++Index;
+         if (fold.Invoke(item, ref seed)) break;
+      }
+
+      return seed;
+   }
+
+   /// <inheritdoc />
+   [MethodImpl(MethodImplOptions.AggressiveInlining)]
+   public bool TryGetCount (out int count) {
+      count = Str.Length - Index;
+      return true;
+   }
+}
+
+public static partial class Iterator {
+   [Pure] [MethodImpl(MethodImplOptions.AggressiveInlining)]
+   public static Contract<IIterator<char>, StringIterator> Iter (this string str) {
+      return new StringIterator(str);
+   }
+}
